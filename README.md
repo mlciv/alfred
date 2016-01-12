709260354005160ba7ffa0d4317cb28d28060163
jiessie <jiessie.cao@gmail.com>
2016-01-12 01:41:54 -0700

    add propbank

--- README.md
+++ README.md
@@ -16,5 +16,17 @@ Pay attention to two things:
    screen is "screen List"
 2. Button 2 or Button 1, the id may differ in different system, test the script
    and debug the button number please.
+   
+   
+##Propbank-alfred 
+Get the propbank of a verb by using this url. 
+https://verbs.colorado.edu/propbank/framesets-english/ 
+P.S. some verbs also have some other form, v, n, j, e.g. absent can be a j and v.
 
+In this index.html page, the following regex pattern can extract the propbank html info. Then show them in the alfred menu. We can choose form we want and open the correct url in the new window.
+
+```
+p = re.compile(r'\<a href=\"%s-.*?\.html\"\> %s-.*?\.html\<\/a\>' % (originQuery,originQuery) ,re.S)
+title_re = re.compile(r'.*?\<a href=\"(?P<html>.*?.html).*',re.S)
+```
 
---
+++ propbank-alfred/propbank.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import StringIO, gzip, alfred, urllib, urllib2, datetime, re,sys
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+reload(sys) 
+sys.setdefaultencoding('UTF-8')
+originQuery = '{query}'
+#originQuery = 'ability'
+theQuery = urllib.quote_plus(originQuery)
+url = 'https://verbs.colorado.edu/propbank/framesets-english/'
+request = urllib2.Request(url)
+req_open = urllib2.build_opener()
+conn = req_open.open(request)
+req_data = conn.read()
+
+data_stream = StringIO.StringIO(req_data)
+actual_data = data_stream.read()
+
+p = re.compile(r'\<a href=\"%s-.*?\.html\"\> %s-.*?\.html\<\/a\>' % (originQuery,originQuery) ,re.S)
+title_re = re.compile(r'.*?\<a href=\"(?P<html>.*?.html).*',re.S)
+items = p.findall(actual_data)
+results = []
+
+if len(items) > 0 :
+	index = 0
+	for item in items:
+                #print item
+		m = title_re.match(item)
+		html = m.group('html')
+                title = html.split('.')[0]
+                subtitle = 'test'
+		aitem = alfred.Item({'uid': index, 'arg' : url+html}, title, subtitle)
+		results.append(aitem)
+		index += 1
+else :
+	aitem = alfred.Item({'uid': 1, 'arg' : url}, 'Not Found', '')
+	results.append(aitem)
+
+xml = alfred.xml(results)
+alfred.write(xml)
